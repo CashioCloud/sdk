ef921168fbd421e14dcdb793cf43f3ac4084cf25
Cashio <pekekgaming4@gmail.com>
2026-02-28 10:00:27 +0700

    Create affiliateService.js

---
+++ services/affiliateService.js
@@ -0,0 +1,28 @@
+function getLinks(){
+
+    return [
+
+        {
+            name:"Amazon",
+            url:"https://amazon.com"
+        },
+
+        {
+            name:"Shein",
+            url:"https://shein.com"
+        },
+
+        {
+            name:"Booking",
+            url:"https://booking.com"
+        }
+
+    ]
+
+}
+
+module.exports = {
+
+    getLinks
+
+}
